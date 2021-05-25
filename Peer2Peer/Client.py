66eb7c4c5ca3058171a66a45425a968f91457968
James Fleming <Jamesdfleming1@dmail.com>
2021-05-25 16:32:25 -0400

    Just starting

--- Peer2Peer/Client.py
+++ Peer2Peer/Client.py
@@ -16,12 +16,12 @@ Connect to the tracker and then all the peers, and then send and receive blocks 
 
 """
 
-import socket
+import select, sys, queue
+from socket import *
 
 class Client:
     def __init__(self):
-        # port to advertise to other peers
-        self.myPort = 60006
+       
         # do we keep a reference to some "entire block chain object"
         # or just two pointers as follows?
         self.lastConfirmedBlock = None
@@ -30,20 +30,44 @@ class Client:
 
         # dictionary to hold peers, their public keys and port numbers 
         self.peers = {}
+        self.inputs = [sys.stdin]
+        self.outputs = []
+        self.address = 'localhost'
+        self.myPort = 60002
+
+        # Create a 
+
+        
 
     
     # create a TCP connection with the tracker. 
-    def connectToTracker():
+    def connectToTracker(self):
         pass
 
 
     # given a peer from the tracker, open a TCP connection with this peer. 
-    def connectToPeer():
+    def connectToPeer(self):
         pass
+
+    def joinChain(self):
+        """Creates a TCP connection with a given tracker 
+            by sending local ip and portnumber and public key 
+        Receives a list of neighbors from the Tracker
+        Opens a TCP with each of them and keeps track of that connection and their 
+        respective public keys"""
+
+
+    def runClient(self):
+        while True:
+            select.select(self.inputs, self.outputs, self.inputs)
+        # run the actually client logic
+
+
+
 
 
     # uses Block Api to first mine, and then attempt to send a block to be included in the block chain
-    def sendBlock():
+    def sendBlock(self):
         # if i receive a block during my attempt to mine and send a block what happens? 
         # do i give up on sending that block and just try to send another block? I mean i have to stop mining right? cause now what im hashing on
         # specifically "last hash" has changed. So do i just keep trying to get my message out over and over again until finally I get lucky enough and mine before anyone else. 
@@ -51,13 +75,13 @@ class Client:
 
         pass
 
-    def receiveBlock():
+    def receiveBlock(self):
         # this gets really tricky i think? Do we immediately assume its correct and then discard if we later see a longer fork?
         # how does a fork actually work? multiple blocks point the the same block as a 'previous' 
         # When do we discard 
         pass 
 
-    def displayBlockChain():
+    def displayBlockChain(self):
         # I'm not sure how actual implementations of block chain / bitcoin tally the entire ledger / blockchain. Im pretty sure they use clever data stuctures / or just someone keeps track of their balance from the get-go 
         # but we need some way of displaying the entire block chain data... Maybe we only need to iterate over blocks we haven't iterated over and always keep track of the last block number that we have already tallied. 
 
@@ -65,7 +89,7 @@ class Client:
 
 
     # Generate private and public key pair so that we can sign our transactions and verify them... However maybe not necessary for the scope of the assignment
-    def generateKeys():
+    def generateKeys(self):
         # most likely would just use an existing API for this 
        #  https://pycryptodome.readthedocs.io/en/latest/src/public_key/rsa.html
         pass
