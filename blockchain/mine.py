faec4415068ddfbb4b9e1e78f3329b316d8d74c7
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2021-05-27 23:50:45 -0700

    simplified loop structure

--- blockchain/mine.py
+++ blockchain/mine.py
@@ -2,9 +2,12 @@ import rsa
 import hashlib
 import math #for faster bitmask calculation
 
-# Collection of APIs used by client in mining mode (miner)
-# miner takes a single block and maintains a copy of the blockchain
-# mining is the process of using rsa to find a hash for the block
+''' Collection of APIs used by client in mining mode (miner)
+ miner takes a single block and maintains a copy of the blockchain
+ mining is the process of using rsa to find a hash for the block '''
+
+#TODO FIX: get hash correctly (sha256 somehow?), use rightshift and AND operator for isMined (sep function), add error checking
+# what is the hash? a string? maybe cast to a string 
 # TODO: add recognition to re-start if another miner finds the hash first
 
 #inputs: new block of transactions and hash padding
@@ -19,18 +22,12 @@ def mine(block, blockchain, hash_padding):
 	
 	while(!isMined):
 	
-		#computes hash w rsa
-		#using transactions and prevHash
-		hashValue = hashlib.sha256()
+		#computes hash w rsa using transactions and prevHash
+		hashValue = str(hashlib.sha256())
 		
 		
 		#checks if the hashValue is longer than the hash padding
-		#should use shifts and AND operator
-		if (math.log10(hashValue))+1 - hash_padding  > 0): 
-  			#checks with hash padding
-			isMined = (int(hashValue[:hash_padding]) == 0)
-		else:
-  			print('Impossible to hash')
+		isMined = verify(hashValue, hash_padding)
 		
 		#increments nonce by 1 if unsuccessful
 		nonce += nonce
@@ -38,10 +35,8 @@ def mine(block, blockchain, hash_padding):
 	#returns hash value if successful
 	return hashValue
 	
-	def verify(hashValue, bitmask, hash_padding):
-		return hashValue & (1 >> hash_padding)
-		
-		
-		#Good job on: general structure
-	#TODO FIX: get hash correctly (sha256 somehow?), use rightshift and AND operator for isMined (sep function), fix incorrect error checking
-	# what is the hash? a string? maybe cast to a string or use toString or something
+	
+#Do something like return hashValue & (111... or shift >> hash_padding)
+def verify(hashValue, hash_padding):
+	return (int(hashValue[:hash_padding]) == 0)
+	
